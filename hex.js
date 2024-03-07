22fd16c78a65e499fc22b948392097bbff298f4b
Isaac Flores <fisaac151@gmail.com>
2024-03-07 13:10:16 -0800

    added comments

--- hex.js
+++ hex.js
@@ -39,6 +39,7 @@ function populateDropdown() {
     dropdown.prepend(option);
 }
 
+// Checking if a new color was chosen
 setInterval(() => {
     if (dValue != dropdown.value) {
         dValue = dropdown.value;
